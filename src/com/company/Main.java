4fdb98b7b40e29a1576639c6e2dc9abd4d00782e
jb838 <jacob.blasek1220@gmail.com>
2018-11-26 11:22:16 -0500

    fdg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int counter = 0;
+        int array[] = new int[1000];
+
+        while (counter != 100) {
+            int ranNum = ((int) (Math.random() * 100)) + 1;
+            int i;
+
+            System.out.println(ranNum);
+            for (i = 0; i < 100; i++) {
+
+                if (ranNum == array[ranNum]) {
+                    array[ranNum - 1] = ranNum;
+                    counter
+                }
+
+            }
+        }
     }
 }
